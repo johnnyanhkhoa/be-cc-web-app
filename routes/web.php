f64aadac5d6d1802459cbb4da02493c47560526c
johnnyanhkhoa <johnnyanhkhoa@gmail.com>
2025-09-26 16:00:34 +0700

    Laravel route to serve storage files

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/storage/{path}', function ($path) {
+    $filePath = storage_path('app/public/' . $path);
+
+    if (!file_exists($filePath)) {
+        abort(404);
+    }
+
+    $mimeType = mime_content_type($filePath);
+
+    return response()->file($filePath, [
+        'Content-Type' => $mimeType,
+        'Cache-Control' => 'public, max-age=31536000'
+    ]);
+})->where('path', '.*')->name('storage.serve');
