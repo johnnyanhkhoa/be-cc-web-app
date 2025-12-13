a617a2734ceccaf73dee0b6eb3674f0a7101e6c4
johnnyanhkhoa <johnnyanhkhoa@gmail.com>
2025-12-13 09:35:41 +0700

    fix: Remove SerializesModels and support both raw JSON and serialized formats

--- app/Jobs/SendAsteriskLogToCCJob.php
+++ app/Jobs/SendAsteriskLogToCCJob.php
@@ -13,7 +13,7 @@ use Carbon\Carbon;
 
 class SendAsteriskLogToCCJob implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable, InteractsWithQueue, Queueable;
 
     public $timeout = 60;
     public $tries = 3;
